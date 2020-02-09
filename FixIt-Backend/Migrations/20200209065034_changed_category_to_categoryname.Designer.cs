c5ebddf6e61940c7ca332bb404b12fb09069b98f
Aakash <aakashamatya63@gmail.com>
2020-02-09 11:36:04 -0600

    sime fix:cahnged category to categoryName

--- FixIt-Backend/Migrations/20200209065034_changed_category_to_categoryname.Designer.cs
+++ FixIt-Backend/Migrations/20200209065034_changed_category_to_categoryname.Designer.cs
@@ -4,14 +4,16 @@ using FixIt_Backend.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FixIt_Backend.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200209065034_changed_category_to_categoryname")]
+    partial class changed_category_to_categoryname
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FixIt-Backend/Migrations/20200209065034_changed_category_to_categoryname.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FixIt_Backend.Migrations
+{
+    public partial class changed_category_to_categoryname : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Categories",
+                table: "Categories",
+                newName: "CategoryName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CategoryName",
+                table: "Categories",
+                newName: "Categories");
+        }
+    }
+}
