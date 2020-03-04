c45a3abbcb5b23920e4740952b1d846d1dd955e5
Aakash <aakashamatya63@gmail.com>
2020-03-03 23:18:01 -0600

    Crud services implemented

---
+++ FixIt-Service/CrudServices/IssueService.cs
@@ -0,0 +1,46 @@
+﻿using FixIt_Data.Context;
+using FixIt_Interface;
+using FixIt_Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FixIt_Service.CrudServices
+{
+    public class IssueService : ICrudService<Issue>
+    {
+        private readonly DataContext context;
+        public IssueService(DataContext context)
+        {
+            this.context = context;
+        }
+        public void Create(Issue model) => context.Issues.Add(model);
+
+
+        public void DeleteById(int id) => context.Issues.Remove(GetById(id));
+
+        public bool Exists(Issue model) => GetById(model.Id) != null;
+
+
+        public IEnumerable<Issue> GetAll() => context.Issues.Include(c => c.Category);
+
+        public Issue GetById(int id) => context.Issues.FirstOrDefault(i => i.Id == id);
+
+        public void Save(Issue model)
+        {
+            if(model.Id == 0)
+            {
+                Create(model);
+            }
+            else
+            {
+                Update(model);
+            }
+        }
+
+        public void Update(Issue model) => context.Issues.Update(model);
+        
+    }
+}
