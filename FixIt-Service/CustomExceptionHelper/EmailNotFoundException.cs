a34f9fac6a026dfcf12716719fe9c56de98f589c
Aakash <aakashamatya63@gmail.com>
2020-03-03 23:18:16 -0600

    Custom exception handler

---
+++ FixIt-Service/CustomExceptionHelper/EmailNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FixIt_Service.CustomExceptionHelper
+{
+    [Serializable]
+    public class EmailNotFoundException : Exception
+    {
+        public EmailNotFoundException()
+            : base("Email not Found.")
+        {
+
+        }
+    }
+}
