2d920bf2a175daf285c5000b05eb7e7871287755
Aakash <aakashamatya63@gmail.com>
2020-03-14 16:30:47 -0500

    change from local host to Ip address

--- FixIt-Backend/Startup.cs
+++ FixIt-Backend/Startup.cs
@@ -155,11 +155,10 @@ namespace FixIt_Backend
             {
                 builder.AllowAnyOrigin()
                        .AllowAnyMethod()
-                       .WithExposedHeaders(new[] { "Content-Range" })
                        .AllowCredentials()
                        .AllowAnyHeader();
             });
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
