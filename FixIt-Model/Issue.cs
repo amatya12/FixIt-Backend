e2d81b4a7f0c15b82e9b750c3f917d03e7d32c20
Aakash <aakashamatya63@gmail.com>
2020-03-14 17:18:30 -0500

    change in issue model

--- FixIt-Model/Issue.cs
+++ FixIt-Model/Issue.cs
@@ -8,25 +8,26 @@ namespace FixIt_Model
         [Key]
         public int Id { get; set; }
 
-        [Column(TypeName ="varchar(500)")]
-        public string Title { get; set; }
+        [Column(TypeName ="varchar(1000)")]
+        public string Issues{ get; set; }
 
-        [Column(TypeName = "varchar(5000)")]
-        public string Description { get; set; }
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        public string Priority { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Location { get; set; }
 
         public double latitude { get; set; }
 
         public double longitude { get; set; }
 
-        public string Severity { get; set; }
-
         public bool Status { get; set; }
 
-        public string ImageUrl { get; set; }
+        
 
-        public string ExtraInformation { get; set; }
-
-        public int CategoryId { get; set; }
-        public virtual Category Category { get; set; }
     }
 }
