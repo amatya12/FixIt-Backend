b13238e1441528933743ab2e096bc66294fb2cc7
Aakash <aakashamatya63@gmail.com>
2020-03-24 02:44:16 -0500

    Added datemodified and changed the data type of date created.

---
+++ FixIt-Data/Migrations/20200324074319_change_datatype_of_DateCreated.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FixIt_Data.Migrations
+{
+    public partial class change_datatype_of_DateCreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateCreated",
+                table: "Issues",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateModified",
+                table: "Issues",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateModified",
+                table: "Issues");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateCreated",
+                table: "Issues",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+        }
+    }
+}
